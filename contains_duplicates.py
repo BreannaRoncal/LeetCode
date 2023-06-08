e07deaec743976fd858e0297d91ac766ca03e084
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2023-06-08 12:30:25 -0700

    Update contains_duplicates.py

--- contains_duplicates.py
+++ contains_duplicates.py
@@ -2,6 +2,18 @@
 Contains Duplicates
 
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Solution that uses less memory by sorting the list first:
+
+    class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        print(nums)
+        for i in range(0, len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+        
 """
 
 class Solution:
